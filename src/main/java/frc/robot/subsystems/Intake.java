269fdf6ca2368c1f5980dfefad10d9dc3b4074d1
Engineering Admin <EEAdmin@chaminet.net>
2020-01-09 16:50:25 -0800

    Intake update

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -9,16 +9,29 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import frc.robot.commands.Base.DriveWithJoysticks;
+
 public class Intake extends SubsystemBase {
   /**
    * Creates a new Intake.
    */
+  private final TalonSRX Intake;
+  public static final int KIntakeTalon = 1; 
+  
   public Intake() {
-
+    Intake = new TalonSRX(KIntakeTalon);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+  public void TakeBall(double IntakeSpeed){
+    Intake.set(ControlMode.PercentOutput, IntakeSpeed);
   }
 }
