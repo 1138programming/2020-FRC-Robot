feac45ba50f608df035e8fc16f8808f8c6b4147d
Engineering Admin <EEAdmin@chaminet.net>
2020-01-19 18:35:24 -0800

    Tilter Code with Motor

---
+++ src/main/java/frc/robot/subsystems/Tilter.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+public class Tilter extends SubsystemBase {
+    private final TalonSRX tilterMotor; 
+    public static final int KTilterTalon = 1; 
+
+    public Tilter (){
+        tilterMotor = new TalonSRX(KTilterTalon); 
+    }
+
+}
