91d27cb1b4cc9155f5379295c5ea6e9b799a8320
Engineering Admin <EEAdmin@chaminet.net>
2020-01-20 10:49:49 -0800

    added EngageStage2.java and DisengageStage2.java

---
+++ src/main/java/frc/robot/commands/Storage/EngageStage2.java
@@ -0,0 +1,40 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.Storage;
+import frc.robot.Robot;
+import frc.robot.subsystems.Storage;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.enums.SolenoidState;
+
+public class EngageStage2 extends CommandBase {
+  
+  public EngageStage2() {
+    addRequirements(Robot.storage);
+  }
+
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    Robot.storage.setShifter(SolenoidState.ACTIVE);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
