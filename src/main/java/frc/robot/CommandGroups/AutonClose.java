acd59ccca7c3029a0bd6f80b871e7591a0e4a1ed
BuildTools <unconfigured@null.spigotmc.org>
2020-02-18 16:24:26 -0800

    Added names for Auton Command Groups

---
+++ src/main/java/frc/robot/CommandGroups/AutonClose.java
@@ -0,0 +1,13 @@
+package frc.robot.CommandGroups;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+import frc.robot.Robot;
+
+public class AutonClose extends SequentialCommandGroup {
+	public AutonClose() {
+        addCommands(
+            
+        );
+	}
+}
