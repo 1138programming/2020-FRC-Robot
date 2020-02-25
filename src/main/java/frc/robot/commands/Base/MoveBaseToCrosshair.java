d0dbba43728614f1a5f8b9ef542ec991788a0bcb
ciherrera20 <ciherrera20@icloud.com>
2020-02-25 15:45:11 -0800

    Added SmartDashboard print to MoveBaseToCrosshair

--- src/main/java/frc/robot/commands/Base/MoveBaseToCrosshair.java
+++ src/main/java/frc/robot/commands/Base/MoveBaseToCrosshair.java
@@ -17,6 +17,7 @@ public class MoveBaseToCrosshair extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    SmartDashboard.putBoolean("Base Aligned", false);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -34,6 +35,7 @@ public class MoveBaseToCrosshair extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    SmartDashboard.putBoolean("Base Aligned", true);
   }
 
   // Returns true when the command should end.
