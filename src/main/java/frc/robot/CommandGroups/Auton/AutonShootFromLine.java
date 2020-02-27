b833ecad773a7b681d5f2a79739e17d9759f42ce
Connor R. Nicholls <CRNicholls20@chaminet.org>
2020-02-27 10:19:11 -0800

    auton hopefully

--- src/main/java/frc/robot/CommandGroups/Auton/AutonShootFromLine.java
+++ src/main/java/frc/robot/CommandGroups/Auton/AutonShootFromLine.java
@@ -4,12 +4,17 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 import frc.robot.Robot;
 import frc.robot.CommandGroups.PositionWithLimelight;
+import frc.robot.commands.Base.MoveBaseFor;
+import frc.robot.commands.Flywheel.SpinUpFlywheel;
 import frc.robot.CommandGroups.FeedShot;
+import frc.robot.commands.Tilter.MoveTilterTo;
 
 public class AutonShootFromLine extends SequentialCommandGroup {
 	public AutonShootFromLine() {
         addCommands(
-            new PositionWithLimelight(),
+            new SpinUpFlywheel(),
+            new MoveBaseFor(1, 1, 500),
+            new MoveTilterTo(61),
             new FeedShot()
         );
     }
