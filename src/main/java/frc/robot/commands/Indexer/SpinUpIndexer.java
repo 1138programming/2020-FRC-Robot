529eeb7e143c5db97c99ca2af812ae7482eaf781
ciherrera20 <ciherrera20@icloud.com>
2020-03-03 08:51:18 -0800

    Finished adding PID to indexer

---
+++ src/main/java/frc/robot/commands/Indexer/SpinUpIndexer.java
@@ -0,0 +1,43 @@
+package frc.robot.commands.Indexer;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Robot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import static frc.robot.Constants.*;
+
+public class IndexIn extends CommandBase {
+  public IndexIn() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(Robot.indexer);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    Robot.indexer.setSetpoint(SmartDashboard.getNumber("Indexer Setpoint", 0.0));
+    Robot.indexer.setConstants(
+        SmartDashboard.getNumber("Indexer P", 0.0),
+        SmartDashboard.getNumber("Indexer I", 0.0), 
+        SmartDashboard.getNumber("Indexer D", 0.0),
+        SmartDashboard.getNumber("Indexer F", 0.0)
+    );
+
+    Robot.indexer.calculate();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -3,13 +3,16 @@ package frc.robot.subsystems;
 import frc.robot.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.*;
-
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import com.revrobotics.*;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANEncoder;
 
 public class Indexer extends SubsystemBase {
   private final CANSparkMax indexer;
+  private final CANEncoder indexerEncoder;
   private PIDController indexerPID;
+  private double indexerPWM = 0;
 
   /**
    * @brief This is the Indexer
@@ -27,12 +30,24 @@ public class Indexer extends SubsystemBase {
     // This protects against wrong configurations if the robot reboots during a match
     indexer.burnFlash();
 
+    indexerEncoder = indexer.getEncoder();
+
     indexerPID = new PIDController(0, 0, 0, 0, 0.02);
+
+    SmartDashboard.putNumber("Indexer Setpoint", 0.0);
+    SmartDashboard.putNumber("Indexer P", indexerPID.getP());
+    SmartDashboard.putNumber("Indexer I", indexerPID.getI());
+    SmartDashboard.putNumber("Indexer D", indexerPID.getD());
+    SmartDashboard.putNumber("Indexer F", indexerPID.getF());
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
+    SmartDashboard.putNumber("Indexer Velocity", getIndexerVel());
+    SmartDashboard.putNumber("Indexer PWM", indexerPWM);
+    SmartDashboard.putNumber("Indexer Error", indexerPID.getError());
   }
   
   /**
@@ -41,7 +56,16 @@ public class Indexer extends SubsystemBase {
    * @param PWM Speed to move the indexer at
    */
   public void move(double PWM) {
+    indexerPWM = PWM;
     indexer.set(PWM);
+  }
+
+  public double getIndexerPosition() {
+    return indexerEncoder.getPosition();
+  }
+
+  public double getIndexerVel() {
+    return indexerEncoder.getVelocity();
   }
 
   public void setSetpoint(double setpoint) {
@@ -49,6 +73,14 @@ public class Indexer extends SubsystemBase {
   }
 
   public void calculate() {
-    //indexerPID.calculate();
+    move(indexerPID.calculate(getIndexerVel()));
+  }
+
+  public boolean atSetpoint() {
+    return indexerPID.atSetpoint();
+  }
+
+  public void setConstants(double kP, double kI, double kD, double kF) {
+    indexerPID.setGains(kP, kI, kD, kF);
   }
 }
